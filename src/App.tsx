7792ea8ec37e2cba50e32a48cdb0f0bc137c9cb9
Neoptolemos Papadiofantous <Neoptolemos.papadiofantous@gmail.com>
2025-07-07 18:24:24 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import FAQ from './pages/FAQ'
 import Contact from './pages/Contact'
 import Terms from './pages/Terms'
 import Privacy from './pages/Privacy'
+import Privacy from './pages/Test'
 
 function App() {
   return (
