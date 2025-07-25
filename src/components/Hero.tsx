6d4ddec284978099bd97495780c4f1db37a97e95
Neoptolemos Papadiofantous <Neoptolemos.papadiofantous@gmail.com>
2025-07-25 22:18:45 +0300

    removed live prices, too much work and not compatible to scale with our current set up. also added dynamic features

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -245,13 +245,6 @@ const Hero = () => {
             ))}
           </div>
         </div>
-
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-textSecondary/30 rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-textSecondary/50 rounded-full mt-2 animate-pulse"></div>
-          </div>
-        </div>
       </div>
     </section>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import Hero from '../components/Hero'
-import LivePrices from '../components/LivePrices'
 import Features from '../components/Features'
 import Stats from '../components/Stats'
 import Newsletter from '../components/Newsletter'
@@ -10,7 +8,6 @@ const Home = () => {
   return (
     <div className="min-h-screen">
       <Hero />
-      <LivePrices />
       <Features />
       <Stats />
       <Newsletter />
