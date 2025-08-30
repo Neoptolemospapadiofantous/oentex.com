122b05bdec13e27c594855dffce7445ce1ca4fa8
Neoptolemos Papadiofantous <Neoptolemos.papadiofantous@gmail.com>
2025-08-30 15:38:19 +0300

    Deleted .env

--- supabase/migrations/20250709131042_increment_deal_clicks_function.sql
+++
@@ -1,9 +0,0 @@
--- supabase/migrations/YYYYMMDDHHMMSS_increment_deal_clicks_function.sql
-CREATE OR REPLACE FUNCTION increment_deal_clicks(deal_id UUID)
-RETURNS void AS $$
-BEGIN
-  UPDATE company_deals 
-  SET click_count = click_count + 1 
-  WHERE id = deal_id;
-END;
-$$ LANGUAGE plpgsql;
